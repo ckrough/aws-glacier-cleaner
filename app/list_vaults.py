d55c17c00d0b7931343cdc30aeb9549486c2e5e9
Chris Krough <461869+ckrough@users.noreply.github.com>
2023-12-23 13:17:27 -0600

    remove trailing whitespace

--- app/list_vaults.py
+++ app/list_vaults.py
@@ -9,11 +9,11 @@ def list_vaults() -> Union[List[str], str]:
     """
     Lists all the vault names in AWS Glacier for the given account.
 
-    Uses CredentialManager to get a Glacier client and lists the vaults. 
+    Uses CredentialManager to get a Glacier client and lists the vaults.
     Handles specific AWS client-related exceptions and logs errors.
 
     Returns:
-        Union[List[str], str]: List of vault names if successful, or 
+        Union[List[str], str]: List of vault names if successful, or
         an error message string in case of failure.
     """
     logger = logging.getLogger(__name__)
